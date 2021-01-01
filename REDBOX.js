08a7d7fdd8be44f38bfda01497e88f5a6dd1adb3
MAYANK5634 <71888040+MAYANK5634@users.noreply.github.com>
2021-01-01 19:08:30 +0530

    Add files via upload

---
+++ REDBOX.js
@@ -0,0 +1,10 @@
+class REDBOX extends BOX {
+    constructor(x,y){
+     super(x,y,20,30);
+    }
+
+    display(){
+     fill("red");
+     super.display();
+     }
+  }
---
+++ YELLOWBOX.js
@@ -0,0 +1,10 @@
+class YELLOWBOX extends BOX {
+    constructor(x,y){
+     super(x,y,20,30);
+    }
+
+    display(){
+     fill(242,217,3);
+     super.display();
+     }
+  }
